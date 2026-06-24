9f5592a392523a0d7910bd485df6a4f80f116489
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-24 16:21:43 +0530

    Setup

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter some input :");
+        int rollno = input.nextInt();
+        System.out.println("Your rollno is : "+ rollno);
+    }
+}
