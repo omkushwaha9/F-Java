00ae18bc32ead671794e286e5e0b5b22159d2b18
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-24 16:39:40 +0530

    Marks

--- src/Input.java
+++ src/Input.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter some input :");
+        System.out.print("Enter some input : ");
         int rollno = input.nextInt();
         System.out.println("Your rollno is : "+ rollno);
-
+        System.out.print("Enter your Marks : ");
+        float marks = input.nextFloat();
+        System.out.println("Your Marks is : "+ marks);
     }
 }
