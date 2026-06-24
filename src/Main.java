c043e0f5deefe537463f5539ff994bfa21d321fc
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-24 16:14:42 +0530

    Setup

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-  public class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        Scanner input = new Scanner(System.in);
     }
   }
 
---
+++ src/primitives.java
@@ -0,0 +1,10 @@
+public class primitives{
+    public static void main(String[] args){
+        int rollno = 002;
+        char letter = 'A';
+        float marks = 98.51f;
+        double LargerDecimalNumbers = 893273784.23134;
+        long largeInteger = 3274534554353534952L ;
+        boolean check =false;
+    }
+}
