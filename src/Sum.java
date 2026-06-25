c26b75f0d83d3611ae6954c72994086b61a6d3b8
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-25 18:34:37 +0530

    Marks

--- src/Sum.java
+++ src/Sum.java
@@ -1,8 +1,13 @@
+import java.net.SocketOption;
 import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args){
         Scanner input =  new Scanner(System.in);
+        // Type Casting
+        int num = (int) (67.89);
+        System.out.println(num);
+        // Sum
         System.out.print("Enter num1 Value : ");
         int num1 = input.nextInt();
         System.out.print("Enter num2 Value : ");
@@ -10,6 +15,8 @@ public class Sum {
 
         int sum = num1 + num2;
         System.out.println("The sum is : "+ sum);
+
+
 
     }
 }
