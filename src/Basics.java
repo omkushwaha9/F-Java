b1f1a056b79f6e7c469e0ee469e21e30d600714a
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-25 19:25:09 +0530

    Marks

--- src/Basics.java
+++ src/Basics.java
@@ -10,6 +10,21 @@ public class Basics {
 //            System.out.print("Hello World!");
 //        }
 
+        // While loop
 
+//        int count = 0;
+//        while (count != 10){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // for loop
+
+//        for (int count = 1; count <= 10; count++){
+//            System.out.println(count);
+//        }
+        for (int i = 1; i <=9; i++){
+            System.out.println(i);
+        }
     }
 }
