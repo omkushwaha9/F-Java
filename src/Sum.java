7c112212276736bb58e6682d241e18126dcbb37c
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-25 18:53:17 +0530

    Marks

--- src/Sum.java
+++ src/Sum.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args){
         Scanner input =  new Scanner(System.in);
-        // Type Casting
-        int num = (int) (67.89);
-        System.out.println(num);
         // Sum
         System.out.print("Enter num1 Value : ");
         int num1 = input.nextInt();
---
+++ src/TypeCasting.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // Type Casting
+//        int num = (int) (67.89f);
+//        System.out.println(num);
+
+//        byte a = 40;
+//        byte b = 50;
+//        byte c = 100;
+//        int d = a * b / c;
+
+//        System.out.println(d);
+
+//        byte b = 50;
+//      b = b * 2;
+
+//        int number = 'A' ;
+//        System.out.print(number);
+
+    }
+}
