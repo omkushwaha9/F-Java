60f0be8778628fbacf6eb206e9e2f41685814c33
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-25 19:06:39 +0530

    Marks

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -23,5 +23,15 @@ public class TypeCasting {
         // compiler always gives you the output in higher value as the decimal(float) is bigger then integer
 //        System.out.println( 3 * 4.7);
 
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = 0.1234;
+        double result = (f * b) + (i / c) -  (d * s);
+        // float + int - double = double
+        System.out.println((f * b) +" "+ (i / c) + " " +  (d * s));
+        System.out.println(result);
     }
 }
