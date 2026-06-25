5128cf112c1afb9b930c967ae46f50ab7ec7fc6c
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-25 19:13:53 +0530

    Marks

--- src/Basics.java
+++ src/Basics.java
@@ -1,7 +1,13 @@
 public class Basics {
     public static void main(String[] args) {
+
 //        if (true){
 //            System.out.println("Hello World!");
+//        }
+
+//        int a = 10;
+//        if (a == 10) {
+//            System.out.print("Hello World!");
 //        }
 
 
