43b2ad39508d4886e4ce284181025d9b62e09766
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-29 14:55:05 +0530

    Marks

--- src/Conditions_Loops/Loops.java
+++ src/Conditions_Loops/Loops.java
@@ -1,5 +1,7 @@
 package Conditions_Loops;
 
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
         // Syntax of For Loops
@@ -8,8 +10,17 @@ public class Loops {
         *- // body
         *- }
         * */
+        // Q= print from 1 to 5
+        /*
         for (int num= 1; num <= 9; num+= 1){
             System.out.println(num);
+        } */
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        // num += 2 means 1+2=3 3+2=5 5+2=7   number = number + 2 = (num+= 2)
+        for (int num = 1; num <= n; num++) {
+            // System.out.print(num + " ");
+            System.out.println("Hello World"); // to print Hello World (n numbers of time)
         }
     }
 }
