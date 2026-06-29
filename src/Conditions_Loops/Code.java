f6a3f1c14e334a15a6c7519de29300cd396c2249
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-29 14:29:35 +0530

    Marks

--- src/Conditions_Loops/Code.java
+++
@@ -1,9 +0,0 @@
-package Conditions_Loops;
-
-import java.util.Scanner;
-
-public class Code {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-    }
-}
