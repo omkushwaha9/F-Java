4b0b6a872e985d1a913eab1178f6f546fcdbfb6f
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-29 15:05:31 +0530

    While Loops

--- src/Conditions_Loops/Loops.java
+++ src/Conditions_Loops/Loops.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Loops {
     public static void main(String[] args) {
+
         // Syntax of For Loops
+
         /*
         *- for ( initialization; condition; increment/Decrement){
         *- // body
@@ -15,12 +17,29 @@ public class Loops {
         for (int num= 1; num <= 9; num+= 1){
             System.out.println(num);
         } */
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        // num += 2 means 1+2=3 3+2=5 5+2=7   number = number + 2 = (num+= 2)
-        for (int num = 1; num <= n; num++) {
+//        Scanner input = new Scanner(System.in);
+//        int n = input.nextInt();
+//        // num += 2 means 1+2=3 3+2=5 5+2=7   number = number + 2 = (num+= 2)
+//        for (int num = 1; num <= n; num++) {
             // System.out.print(num + " ");
-            System.out.println("Hello World"); // to print Hello World (n numbers of time)
+//            System.out.println("Hello World"); // to print Hello World (n numbers of time)
+//        }
+
+        // While Loop
+
+        /*
+           Syntax :
+             while (Condition){
+                   // body
+             }
+        */
+        //  for (int num= 1; num <= 9; num+= 1){
+        //      System.out.println(num);
+        int num = 1;
+        while (num <= 10 ){
+            System.out.println(num);
+            num += 1;
         }
+
     }
 }
