555091ee27a6bd9b79a051a2a86a484b287cadc4
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-29 14:41:26 +0530

    Marks

--- src/Conditions_Loops/Loops.java
+++ src/Conditions_Loops/Loops.java
@@ -2,6 +2,14 @@ package Conditions_Loops;
 
 public class Loops {
     public static void main(String[] args) {
-        
+        // Syntax of For Loops
+        /*
+        *- for ( initialization; condition; increment/Decrement){
+        *- // body
+        *- }
+        * */
+        for (int num= 1; num <= 9; num+= 1){
+            System.out.println(num);
+        }
     }
 }
