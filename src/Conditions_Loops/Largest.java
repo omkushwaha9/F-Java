658e1ed2ad007713a6ba417089698d9e65e07f16
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-07-01 21:58:51 +0530

    While Loops

---
+++ src/Conditions_Loops/Largest.java
@@ -0,0 +1,14 @@
+package Conditions_Loops;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+
+    }
+}
